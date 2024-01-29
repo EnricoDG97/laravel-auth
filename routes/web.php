40a89dc26d5ba87a809a165ddc58673437bc5a65
EnricoDG97 <enrico.delgaudio97@gmail.com>
2024-01-29 21:31:32 +0100

    feat: added correct routes to admin panel, created model migration and seeder (with faker) of projects

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,21 +19,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
 
-// Route::middleware('auth', 'verified')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 // });
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth', 'verified'])
+    // nome rotta visibile in route list: admin. + nome rotta
+    ->name('admin.')
+    // il prefisso invece è per l'url
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-});
+        Route::get('/messages', function() {
+            return 'messages';
+        })->name('messages');
+        // inserire qui altre srotte
+    });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
